75b93ce9a76c0484fbf8b373a1229a19f853a7b1
aksharakra <akshakaka@gmail.com>
2015-10-22 13:18:48 +0530

    Chapter 5 added

---
+++ C/SkillDevelopmentArea/05-Conditional-Operators/nested-if-else.c
@@ -0,0 +1,24 @@
+//We can write an entire if else construct within either body of the if statement or the body of an else statement
+. This is called nesting of ifs.
+ 
+#include<stdio.h>
+
+int main(void)
+ {
+   int i;
+   
+   printf("Enter either 1 or 2");
+   scanf("%d",&i);
+
+   if(i==2)
+       printf("You'll go to USA\n");
+
+   else
+   {
+       if(i==2)
+           printf("LA is waiting for you");
+       else
+           printf("What about Ibiza");
+   }
+ return 0;
+ }
---
+++ C/SkillDevelopmentArea/05-Conditional-Operators/the-ternary-operators.c
@@ -0,0 +1,20 @@
+//*The conditional operators ? and : are sometimes called ternary operators since they take three arguments. 
+syntax for the same is expression 1 ? expression 2 : expression 3.
+it can be used instead of if else.*//
+
+#include<stdio.h>
+
+int main(void)
+ {
+   int i;
+
+   scanf("%d",&i);
+   (i==1 ? printf("Amit") : printf("All and sundry"));
+ 
+   return 0;
+
+ }
+
+
+//*If i=1, then Amit will appear on screen.
+If i!=0, then All and sundry will appear on screen.*// 
